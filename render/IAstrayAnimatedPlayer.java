68c476b3d12fec11b2d2d2938b7d459e218635b1
thatoneaiguy <71119717+thatoneaiguy@users.noreply.github.com>
2024-09-07 13:09:58 +0100

    Delete render directory

--- render/IAstrayAnimatedPlayer.java
+++
@@ -1,9 +0,0 @@
-package com.thatoneaiguy.archipelago.render;
-
-import dev.kosmx.playerAnim.api.layered.IAnimation;
-import dev.kosmx.playerAnim.api.layered.ModifierLayer;
-
-public interface IAstrayAnimatedPlayer {
-    ModifierLayer<IAnimation> archipelago_getModAnimation();
-}
-
